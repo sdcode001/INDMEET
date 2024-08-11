147e657bf2de865355818a96eed8809f2dea6739
Souvik Dey <deysouvik700@gmail.com>
2024-08-12 01:37:34 +0530

    code update3 v1

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -37,7 +37,7 @@ const MeetingPage = () => {
         {!isSetupComplete ? (
           <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
         ) : (
-          <MeetingRoom meetingId={id}/>
+          <MeetingRoom meetingId={id.toString()}/>
         )}
         </StreamTheme>
       </StreamCall>
